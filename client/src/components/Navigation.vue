e8806fd88e3224a605ed804d23a22450c844effd
Jovan Gliguroski <gliguroski.jovan@gmail.com>
2022-05-15 13:35:20 +0200

    lista + routes

---
+++ client/src/components/Navigation.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="container">
+    <header class="d-flex flex-wrap justify-content-center py-3 mb-4 border-bottom">
+      <a href="/" class="d-flex align-items-center mb-3 mb-md-0 me-md-auto text-dark text-decoration-none">
+        <span class="fs-3">ČVUT News</span>
+      </a>
+
+      <ul class="nav nav-pills">
+        <li class="nav-item">
+          <router-link class="nav-link text-dark" to="/home">
+            <em class="bi-house"></em> Home
+          </router-link>
+        </li>
+        <li class="nav-item">
+          <router-link class="nav-link text-dark" to="/about">
+            <em class="bi-info-circle-fill"></em> About
+          </router-link>
+        </li>
+        <li class="nav-item">
+          <router-link class="nav-link text-dark" to="/narticle">
+            <em class="bi-plus-circle"></em> New article
+          </router-link>
+        </li>
+      </ul>
+    </header>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Navigation"
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ client/src/views/NewArticleView.vue
@@ -0,0 +1,13 @@
+<template>
+
+</template>
+
+<script>
+export default {
+  name: "NewArticleView"
+}
+</script>
+
+<style scoped>
+
+</style>
